0a36cd5a753b1c1cd25a61ef18d90278c3bd9734
maksimetny <maksimetny@gmail.com>
2020-05-12 23:56:58 +0300

    index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 
 /*
     MCLL core export
-    https://github.com/maksimetny/MinecraftLauncherLib
+    https://github.com/maksimetny/minecraft-launcher-lib
 */
 
+export * from './components/launcher'
+export * from './components/downloader'
+export * from './components/user'
+export * from './components/util'
+export * from './components/version'
 export { urls, events } from './constants'
