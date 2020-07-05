58ba8f2790b6416b7fedf784e9fba14e429eb8c8
maksimetny <maksimetny@gmail.com>
2020-07-05 21:36:58 +0300

    nullish coalescence in argument formatting method

--- src/components/version/arg.ts
+++ src/components/version/arg.ts
@@ -48,9 +48,7 @@ export class Argument {
 
     static format(template: string, fields: Map<string, string>) {
         return template.replace(/\$\{(.*?)}/g, key => {
-            const _key = key.substring(2).substring(0, key.length - 3)
-            // console.log(_key, fields.has(_key))
-            return fields.has(_key) ? fields.get(_key) ?? key : key
+            return fields.get(key.substring(2).substring(0, key.length - 3)) ?? key
         })
     } // https://github.com/voxelum/minecraft-launcher-core-node/blob/3d5aa7a38cbc66cdfc9b9d68a8bdf4988905cb72/packages/core/launch.ts
 
