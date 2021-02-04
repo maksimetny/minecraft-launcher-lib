9f0436d966a8be7183fe59500909e1630ae1528f
Dmitry Maksimetny <maksimetny@gmail.com>
2021-02-04 10:29:10 +0300

    babel

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,30 @@
 const gulp = require('gulp')
 const ts = require('gulp-typescript')
 const sm = require('gulp-sourcemaps')
+const fs = require('fs-extra')
 const project = ts.createProject('tsconfig.json')
+const babel = require('gulp-babel')
+const merge = require('merge2')
 
-gulp.task('default', () => {
-    return project
+gulp.task('build', () => {
+    const result = project
         .src()
         .pipe(sm.init())
         .pipe(project())
-        .pipe(sm.write('.', {
-            sourceRoot: './',
-            includeContent: false,
-        }))
-        .pipe(gulp.dest("dist"))
+    return merge([
+        result
+            .js
+            .pipe(babel()),
+        result
+            .dts,
+    ])
+        .pipe(sm.write('.', { sourceRoot: './', includeContent: false }))
+        .pipe(gulp.dest('dist'))
 })
+
+gulp.task('clean', async done => {
+    await fs.emptyDir('dist')
+    done()
+})
+
+gulp.task('default', gulp.series('clean', 'build'))
