21d3420978cc6d1d99448b50538b29afd2346c85
Dmitry Maksimetny <maksimetny@gmail.com>
2021-02-06 14:23:56 +0300

    update lib example

--- src/examples/lib.ts
+++ src/examples/lib.ts
@@ -1,23 +1,21 @@
 
+import { resolve } from 'path'
+
 import {
     Library,
     LibraryDownloads,
 } from '../index'
 
-import { resolve } from 'path'
-
 import {
     readJson,
 } from 'fs-extra'
 
-const lib_1 = Library.from({ name: 'com.mojang:patchy:1.1' })
-console.log(lib_1)
+(async () => {
+    const minLib = Library.from({ name: 'com.mojang:patchy:1.1' })
+    console.log(minLib)
 
-const lib_2_path = resolve('mock', 'libraries', 'org', 'lwjgl', 'lwjgl-openal', '3.2.2', 'lwjgl-openal-3.2.2.json')
-readJson(lib_2_path).then(lib_2_json => {
-    const lib_2 = Library.from(lib_2_json)
-    console.log(lib_2)
-    console.log(lib_2.downloads.classifiers)
-}).catch(err => {
-    console.error(err)
-})
+    const libPath = resolve('mock', 'libraries', 'org', 'lwjgl', 'lwjgl-openal', '3.2.2', 'lwjgl-openal-3.2.2.json')
+    const lib = Library.from(await readJson(libPath))
+    console.log(lib)
+    console.log(lib.downloads.classifiers)
+})().catch(err => console.error(err))
