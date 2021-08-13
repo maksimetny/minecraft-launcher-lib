427c220fd7258166614972b93649fc8820cfda1e
maksimetny <maksimetny@gmail.com>
2021-08-13 15:48:21 +0300

    added gulp lint task

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,20 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const sm = require('gulp-sourcemaps');
+const es = require('gulp-eslint');
 const fs = require('fs-extra');
 const babel = require('gulp-babel');
 const merge = require('merge2');
 
 const project = ts.createProject('tsconfig.json');
+
+gulp.task('lint', () => {
+    return project
+        .src()
+        .pipe(es())
+        .pipe(es.format())
+        .pipe(es.failAfterError());
+});
 
 gulp.task('build', () => {
     const result = project
