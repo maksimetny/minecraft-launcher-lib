f88923939143d98c9198cb58cbc0a520ecc4ef0d
maksimetny <maksimetny@gmail.com>
2021-10-08 18:26:07 +0300

    fix readJson

--- src/util.ts
+++ src/util.ts
@@ -3,4 +3,4 @@ import { readFile } from 'fs';
 import { promisify } from 'util';
 
 export const read = promisify(readFile);
-export const readJson = async <J>(path: string, encoding = 'utf-8'): Promise<J> => JSON.parse(await read(path, encoding));
+export const readJson = async <J>(path: string, encoding: BufferEncoding = 'utf-8'): Promise<J> => JSON.parse(await read(path, encoding));
